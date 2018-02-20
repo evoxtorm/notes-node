4af16cb4c788340e8c317baaba7530fb0e751547
evoxtorm <evostorm96@gmail.com>
2018-02-21 00:39:11 +0530

    Add new utility

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const os = require('os');
 const _  = require('lodash');
 const notes = require('./notes.js');
 
-console.log(_.isString(true));
-console.log(_.isString('Hitesh'));
+let filteredArray = _.uniq(['Hitesh',1, 'Hitesh',1,2,3,4]);
+console.log(filteredArray);
+
+// console.log(_.isString(true));
+// console.log(_.isString('Hitesh'));
 
 let user = os.userInfo();
 

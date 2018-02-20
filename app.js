0e9357ad3766514f76d8ce6aaa82f888c9c0cda0
evoxtorm <evostorm96@gmail.com>
2018-02-21 00:33:19 +0530

    Added lodash library

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-console.log('Starting app.');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _  = require('lodash');
+const notes = require('./notes.js');
+
+console.log(_.isString(true));
+console.log(_.isString('Hitesh'));
 
 let user = os.userInfo();
 
---
+++ notes.js
@@ -0,0 +1,6 @@
+console.log("Starting notes.js");
+
+
+module.exports.add = (a,b) => {
+  return a+b ;
+}

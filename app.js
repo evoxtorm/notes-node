73c0587a8469008f0717dcfed04a8cba3e260b78
evoxtorm <evostorm96@gmail.com>
2018-02-21 19:55:14 +0530

    Adding Yargs

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
+// const os = require('os');
 const _  = require('lodash');
+const yargs = require('yargs');
+
 const notes = require('./notes.js');
 
-let filteredArray = _.uniq(['Hitesh',1, 'Hitesh',1,2,3,4]);
-console.log(filteredArray);
+var command = process.argv[2];
+console.log('Command: ', command);
+console.log(process.argv);
 
-// console.log(_.isString(true));
-// console.log(_.isString('Hitesh'));
+if (command === 'add') {
+  console.log('Adding new note');
+} else if (command === 'list') {
+  console.log('Adding list!');
+} else if (command === 'read') {
+  console.log('Read all!');
+} else if (command === 'remove') {
+  console.log('Removing list!');
+} else {
+  console.log('Command not recognized');
+}
 
-let user = os.userInfo();
-
-fs.appendFileSync('greeting.txt', `Hello ${user.username}!`);
+// let filteredArray = _.uniq(['Hitesh',1, 'Hitesh',1,2,3,4]);
+// console.log(filteredArray);
+//
+// // console.log(_.isString(true));
+// // console.log(_.isString('Hitesh'));
+//
+// let user = os.userInfo();
+//
+// fs.appendFileSync('greeting.txt', `Hello ${user.username}!`);
--- notes.js
+++ notes.js
@@ -1,6 +1,1 @@
 console.log("Starting notes.js");
-
-
-module.exports.add = (a,b) => {
-  return a+b ;
-}

eed1102c1edc979be576b3893ae35f371712eebc
evoxtorm <evostorm96@gmail.com>
2018-02-22 23:59:00 +0530

    GetALL

--- notes.js
+++ notes.js
@@ -5,6 +5,10 @@ let addNote = (title, body) => {
   console.log('Adding note', title, body);
 };
 
+let getALl = () => {
+  console.log('Getting all notes');
+};
+
 module.exports = {
   addNote
 };

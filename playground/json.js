0c68a17c5dc89a5d70c10bad7cebbb6b60394f4e
evoxtorm <evostorm96@gmail.com>
2018-02-27 22:25:07 +0530

    Workinh with Json

--- playground/json.js
+++ playground/json.js
@@ -5,3 +5,28 @@
 // let stringObj = JSON.stringify(obj);
 // console.log(typeof stringObj);
 // console.log(stringObj);
+ // let personString = '{"name": "Hitesh","age" : 21}';
+ // let person = JSON.parse(personString);
+ // console.log(typeof person);
+ // console.log(person);
+
+const fs = require('fs');
+
+let originalNote = {
+  title: 'Some title',
+  body: 'Some body'
+};
+
+let originalNoteString = JSON.stringify(originalNote);
+
+// originalNoteString
+fs.writeFileSync('notes.json', originalNoteString);
+
+let noteString = fs.readFileSync('notes.json');
+//note
+
+let note = JSON.parse(originalNoteString);
+
+console.log(typeof note);
+console.log(note);
+console.log(note.title);

6903b5c0035e6758d8fcc4346c0585c4a20c6d90
evoxtorm <evostorm96@gmail.com>
2018-02-28 20:26:13 +0530

    fs

--- notes.js
+++ notes.js
@@ -1,5 +1,7 @@
 console.log("Starting notes.js");
 
+const fs = require('fs');
+
 
 let addNote = (title, body) => {
   // console.log('Adding note', title, body);
@@ -8,6 +10,9 @@ let addNote = (title, body) => {
     title,
     body
   };
+
+  notes.push(note);
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
 };
 
 let getALl = () => {

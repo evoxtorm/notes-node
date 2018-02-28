73878cc4afcd59ec7bb69a330280b0a92a8e8827
evoxtorm <evostorm96@gmail.com>
2018-02-28 21:40:13 +0530

    Try & Catch

--- notes.js
+++ notes.js
@@ -11,11 +11,19 @@ let addNote = (title, body) => {
     body
   };
 
-  let notesString = fs.readFileSync('notes-data.json');
-  notes = JSON.parse(notesString);
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+  } catch (e) {
 
+  }
+
+var duplicateNotes = notes.filter((note) => note.title === title);
+
+if (duplicateNotes.length === 0) {
   notes.push(note);
   fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
 };
 
 let getALl = () => {

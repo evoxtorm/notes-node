02c9fb05ea5ce9352b836f19af78870387264e0e
evoxtorm <evostorm96@gmail.com>
2018-03-03 00:30:15 +0530

    dbugger

--- notes.js
+++ notes.js
@@ -60,6 +60,7 @@ let removeNotes = (title) => {
 
 
 var logNote = (note) => {
+  debugger;
   console.log("--");
   console.log(`Title  ${note.title}`);
   console.log(`Body  ${note.body}`);
---
+++ playground/debugging.js
@@ -0,0 +1,11 @@
+let person = {
+  name : 'Hitesh'
+};
+
+person.age = 25 ;
+
+
+debugger ;
+person.name = 'Lol' ;
+
+console.log(person);
